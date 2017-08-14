0996cf0deaf1b7cfc4ecdc3306264e28c26af6c3
EM-Lee <ten4han@gmail.com>
2017-08-14 12:04:18 -0400

    Add files via upload

---
+++ for Spatial Analysis and Mapping/2.2.R
@@ -0,0 +1,28 @@
+tree.heights <- c(4.3, 7.1, 6.3, 5.2, 3.2, 2.1)
+
+tree.heights
+tree.heights ** 2
+sum(tree.heights)
+mean(tree.heights)
+max.height <- max(tree.heights)
+max.height
+
+tree.heights
+tree.heights[1]
+tree.heights[1:3]
+sqrt(tree.heights[1:3])
+tree.heights[c(5, 3, 2)]
+# tree.heights[5, 3, 2]
+# tree.heights[5 3 2]
+
+name <- "Lex Comber"
+name
+cities <- c("Leicester", "Newcastle", "London", "Durham", "Exeter")
+cities
+length(cities)
+northern <- c(FALSE, TRUE, FALSE, TRUE, FALSE)
+northern
+cities[northern]
+
+install.packages("GISTools")
+library("GISTools")
